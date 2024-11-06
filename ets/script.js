2aa2049612e8813b5f7a7a7a5e5a1b5729a39c16
Happpizh <162117394+Happpizh@users.noreply.github.com>
2024-11-06 19:23:20 +0700

    Update script.js

--- ets/script.js
+++ ets/script.js
@@ -1,9 +1,13 @@
+const filmpoint = document.querySelector('.grid-film');
+
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
-    }
-  });
-}, { threshold: 0.5 });
+   entries.forEach(entry => {
+     if (entry.isIntersecting) {
+       entry.target.classList.add('show');
+     } else {
+       entry.target.classList.remove('show');
+     }
+   });
+ }, { threshold: 0.5 });
+
+observer.observe(filmpoint);
